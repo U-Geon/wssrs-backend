ca183ceb0ab3f68447f55561bf0b11c98d48483a
U-Geon <sksmsfbrjs@naver.com>
2024-07-10 17:52:29 +0900

    project start

---
+++ wssrs/src/main/java/bootcamp/wssrs/WssrsApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.wssrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WssrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WssrsApplication.class, args);
+	}
+
+}
---
+++ wssrs/src/test/java/bootcamp/wssrs/WssrsApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.wssrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WssrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
